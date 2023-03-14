06b105b7f2a89e2fa5470a99f916df0a953bb281
ctneal91 <ctnealherman@gmail.com>
2023-03-14 10:33:25 -0500

    Add Activity validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ApplicationRecord
-	# validates :name, presence: true
-	# validates :category, presence: true
-    # validates :participants, presence: true, numericality: { only_integer: true, in: (1..8) }
-    # validates :price, presence: true, numericality: { in: (0..1)}
-    # validates :accessibility, presence: true, numericality: { in: (0..1)}
+	validates :name, presence: true
+	validates :category, presence: true
+    validates :participants, presence: true, numericality: { only_integer: true, in: (1..8) }
+    validates :price, presence: true, numericality: { in: (0..1)}
+    validates :accessibility, presence: true, numericality: { in: (0..1)}
 end
