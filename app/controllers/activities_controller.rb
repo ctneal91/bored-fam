62faf8087359c5a9f753bf90ff440ed59df6c488
ctneal91 <ctnealherman@gmail.com>
2023-03-14 09:17:00 -0500

    Add create/new for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,4 +6,23 @@ class ActivitiesController < ApplicationController
   def show
     @activity = Activity.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @activitiy = Activity.new(activity_params)
+
+    if @activity.save
+      redirect_to @activity
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def activity_params
+      params.require(:activity).permit(:name, :type, :participants, :price, :accessibility)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "activities#index"
 
-  get "/activities", to: "activities#index"
-  get "/activities/:id", to: "activities#show"
+  resources :activities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
