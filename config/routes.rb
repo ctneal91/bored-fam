433a32fb1c5dd7c135484f7c5f7923375c82b8f3
ctneal91 <ctnealherman@gmail.com>
2023-03-14 09:00:05 -0500

    Add basic activities show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "activities#index"
 
   get "/activities", to: "activities#index"
+  get "/activities/:id", to: "activities#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
