e3d911ac5ba5086978924813247bffc52bf5a3d7
ctneal91 <ctnealherman@gmail.com>
2023-03-14 15:19:45 -0500

    Add comments resources routes w/i activities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "activities#index"
 
-  resources :activities
+  resources :activities do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
