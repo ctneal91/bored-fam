a5b40cef16316870b31d2e93256a822c689b31a9
ctneal91 <ctnealherman@gmail.com>
2023-03-15 13:56:11 -0500

    Add activity to users

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,11 +2,13 @@ class Activity < ApplicationRecord
     @allowed_categories = %w(charity cooking music diy education social busywork recreational relaxation)
 
     has_many :comments
+    belongs_to :user
     
     validates :name, presence: true
 	validates :category, inclusion: { in: @allowed_categories, message:  "%{value.capitalize} is not a valid category."}, allow_blank: true
     validates :participants, numericality: { only_integer: true, in: (1..8) }, allow_blank: true
     validates :price, numericality: { in: (0..1)}, allow_blank: true
     validates :accessibility, numericality: { in: (0..1)}, allow_blank: true
+    validates :user_id, presence: true
 end
 
