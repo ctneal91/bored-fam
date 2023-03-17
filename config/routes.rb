a03b3187610c646f3b2b2752b9b572e808accfa9
ctneal91 <ctnealherman@gmail.com>
2023-03-17 12:34:38 -0500

    Add password update/edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   # registration & login in routes
-  get 'sign_up', to: 'registrations#new'
-  post 'sign_up', to: 'registrations#create'
-  get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create', as: 'log_in'
-  delete 'logout', to: 'sessions#destroy'
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create", as: "log_in"
+  delete "logout", to: "sessions#destroy"
+
+  #passwords edit/reset
+  get "password", to: "passwords#edit", as: "edit_password"
+  patch "password", to: "passwords#update"
 
   resources :activities do
     resources :comments
